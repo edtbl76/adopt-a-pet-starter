192f523c9308646d4127c1f4eed65ff4b01db48d
Ed Mangini <me@emangini.com>
2022-07-15 22:05:02 -0400

    Add PetDetailsPage component to App

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
   return (
     <Router>
       <Navigation />
+      <Route path="/:type/:id" >
+        <PetDetailsPage />
+      </Route> 
       <Route path="/:type?" >
         <HomePage />
       </Route> 
